abe813a38f4626e2b080e59f648cdeffc49357e0
Brett g Porter <bgp@bgporter.net>
2015-11-05 13:03:43 -0500

    Commenting the test methods.

--- test_post.py
+++ test_post.py
@@ -9,20 +9,26 @@ class TestSlugify(unittest.TestCase):
       pass
 
    def test_simple(self):
+      ''' make sure we go lowercase and handle leading/trailing 
+         whitespace correctly.
+      '''
       self.assertEqual("simple", post.slugify(u"Simple"))
       self.assertEqual("simple", post.slugify(" Simple"))
       self.assertEqual("simple", post.slugify("Simple "))
       self.assertEqual("simple", post.slugify(" Simple "))
 
    def test_dashes(self):
+      ''' spaces replaced with a single dash? '''
       self.assertEqual("simple-test", post.slugify("SIMPLE TEST"))
       self.assertEqual("simple-test", post.slugify("SIMPLE   TEST"))
 
    def test_punctuation(self):
+      ''' punctuation removed, replaced with a single dash like space? '''
       self.assertEqual("remove-the-punctuation", 
          post.slugify("Remove, the? !punctuation..."))
       self.assertEqual("this-that-and-something-else", 
          post.slugify(" This, that, and something else? "))
 
    def test_unicode(self):
+      ''' Unicode converted to acceptable IRI format? '''
       self.assertEqual("m%C3%BCller", post.slugify(u"M\u00fcller"))
