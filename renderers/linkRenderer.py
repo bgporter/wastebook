094604d38490d809817b7e887ebc0a856c5fbaa2
Brett g Porter <bgp@bgporter.net>
2015-11-17 09:12:42 -0500

    Adding tag renderer.
    We'll need to have a function up in the web app portion that knows about
    Flask that can convert out FIX_URL() markup into the correct actual paths
    as part of rendering (and can also do things like attach CSS styles to links
    as appropriate, etc.) After the URLs are updated, re-save the rendered text
    back into the post in the database.

--- renderers/linkRenderer.py
+++ renderers/linkRenderer.py
@@ -1,17 +1,27 @@
 
 
 import re
-import string
 
 import renderer
 
 
 
-STOP = re.escape(r" ,;:.!?")
 LINK_PATTERN = re.compile(r'(^|\s)(http(?:s)?://[^ \<]+\w)')
 
 
 class LinkRenderer(renderer.RenderBase):
+   '''
+      Converts standalone http or https links in text into the Markdown
+      equivalent, so 
+
+      lorem ipsum http://www.example.com, etc
+
+      becomes
+
+      lorem ipsum [http://www.example.com](http://www.example.com), etc
+
+   '''
+
 
    def RenderText(self):
       self.text = LINK_PATTERN.sub('\g<1>[\g<2>](\g<2>)', self.text)
--- renderers/markdownRenderer.py
+++ renderers/markdownRenderer.py
@@ -5,6 +5,10 @@ from markdown import markdown
 import renderer
 
 class MarkdownRenderer(renderer.RenderBase):
+   '''
+      Passes the text through Markdown. Should almost certainly be the
+      last renderer in the pipeline that text gets sent through.
+   '''
 
    def RenderText(self):
       self.text = markdown(self.text)
